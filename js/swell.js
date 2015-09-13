c78a229013ab046d2966e9e5392f0c08b430e82a
lizlove <elizabethlovero@gmail.com>
2015-09-13 13:42:29 -0400

    css with basic slider

--- js/swell.js
+++ js/swell.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-  $('.responsive').slick({
+  $('.carousel').slick({
     dots: true,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -19,7 +19,7 @@ $(document).ready(function(){
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
