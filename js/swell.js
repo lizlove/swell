26745a027a131b0d661a082931078ec834068187
lizlove <elizabethlovero@gmail.com>
2015-09-13 17:52:35 -0400

    finish responsive media and clearfix

--- js/swell.js
+++ js/swell.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-  $('.carousel').slick({
+  $('.responsive').slick({
     dots: true,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 4,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
+          slidesToScroll: 3,
           infinite: true,
           dots: true
         }
@@ -19,7 +19,7 @@ $(document).ready(function(){
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
+          slidesToScroll: 2
         }
       },
       {
