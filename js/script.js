f2d658d6d6dd887ec9fb1eb2bb8ddf2f331bab59
lizlove <elizabethlovero@gmail.com>
2015-09-14 01:19:11 -0400

    api call working, but not appending properly

--- js/script.js
+++ js/script.js
@@ -1,22 +1,7 @@
-// $(document).ready(function() {
-// 	// Get images from mock API and append to image carousel
-// 	var swellAPI = 'http://builds.tryswell.com/mock/items.json';
-// 	$.getJSON(swellAPI, function(result){
-// 		result.items.forEach(function(shoe){
-// 			var id_number = "<h3>Tip " + shoe.id + "<h3/>";
-// 			var url = shoe.images[0]['url'];
-// 			var image = "<img src='" + url + "' class="item" height="200" width="200"/>";
-// 			var title = shoe.heading;
-// 			var copy = "<p>" + shoe.description + "</p>";
-// 			var caption = image + id_number + copy 
-// 			$('.slide').clone().replaceWith(caption).insertAfter(this);
-// 		});
-// 	}); 
-// }); 
-
 $(document).ready(function() {
 	// Build image carousel with responsive structure
-		$('.slider').slick({
+	$('.slider').slick({
+		lazyLoad: 'ondemand',
 		dots: true,
 		arrows: true,
 		infinite: true,
@@ -47,6 +32,20 @@ $(document).ready(function() {
 				}
 			}
 		]
+	});
+
+	// Get images from mock API and append to image carousel
+	var swellAPI = 'http://builds.tryswell.com/mock/items.json';
+	$.getJSON(swellAPI, function(result){
+		result.items.forEach(function(shoe){
+			var id_number = "<h3>Tip " + shoe.id + "<h3/>";
+			var url = shoe.images[0]['url'];
+			var image = "<img src='" + url + "' class='item' height='200' width='200'/>";
+			var title = shoe.heading;
+			var copy = "<p>" + shoe.description + "</p>";
+			console.log(id_number, image, copy);
+			$('.slick-track').append(id_number, image, copy);
+		});
 	}); 
 });
 	 
