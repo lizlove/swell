e376c5ea3ada73c3e6415cea18ef6a8003e2f306
lizlove <elizabethlovero@gmail.com>
2015-09-14 01:01:04 -0400

    carousel with hard-coded images

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
-$(document).ready(function() {
-	// Get images from mock API and append to image carousel
-	var swellAPI = 'http://builds.tryswell.com/mock/items.json';
-	$.getJSON(swellAPI, function(result){
-		result.items.forEach(function(shoe){
-			var id_number = shoe.id;
-			var url = shoe.images[0]['url'];
-			var image = "<div><img src='" + url + "' alt='item'></div>";
-			var title = shoe.heading;
-			var copy = shoe.description;
-			console.log(shoe);
-			console.log(image);
-			$('.slider').append(image)
-		});
-	}); 
-}); 
+// $(document).ready(function() {
+// 	// Get images from mock API and append to image carousel
+// 	var swellAPI = 'http://builds.tryswell.com/mock/items.json';
+// 	$.getJSON(swellAPI, function(result){
+// 		result.items.forEach(function(shoe){
+// 			var id_number = "<h3>Tip " + shoe.id + "<h3/>";
+// 			var url = shoe.images[0]['url'];
+// 			var image = "<img src='" + url + "' class="item" height="200" width="200"/>";
+// 			var title = shoe.heading;
+// 			var copy = "<p>" + shoe.description + "</p>";
+// 			var caption = image + id_number + copy 
+// 			$('.slide').clone().replaceWith(caption).insertAfter(this);
+// 		});
+// 	}); 
+// }); 
 
 $(document).ready(function() {
 	// Build image carousel with responsive structure
