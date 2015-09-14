9fc13ddb515c9ee0137bb587d3113b8326a0366a
lizlove <elizabethlovero@gmail.com>
2015-09-13 23:25:01 -0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// $(document).ready(function() {
-// 	// Get images from mock API and append to image carousel
-// 	var swellAPI = 'http://builds.tryswell.com/mock/items.json';
-// 	$.getJSON(swellAPI, function(result){
-// 		result.items.forEach(function(shoe){
-// 			var id_number = shoe.id;
-// 			var url = shoe.images[0]['url'];
-// 			var image = "<div><img src='" + url + "' alt='item'></div>";
-// 			var title = shoe.heading;
-// 			var copy = shoe.description;
-// 			console.log(shoe);
-// 			console.log(image);
-// 			$('.slider').append(image)
-// 		});
-// 	}); 
-// }); 
+$(document).ready(function() {
+	// Get images from mock API and append to image carousel
+	var swellAPI = 'http://builds.tryswell.com/mock/items.json';
+	$.getJSON(swellAPI, function(result){
+		result.items.forEach(function(shoe){
+			var id_number = shoe.id;
+			var url = shoe.images[0]['url'];
+			var image = "<div><img src='" + url + "' alt='item'></div>";
+			var title = shoe.heading;
+			var copy = shoe.description;
+			console.log(shoe);
+			console.log(image);
+			$('.slider').append(image)
+		});
+	}); 
+}); 
 
 $(document).ready(function() {
 	// Build image carousel with responsive structure
