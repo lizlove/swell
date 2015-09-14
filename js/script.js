e85ccb40c10e97b8dadbfc5a762adfa00bce94f5
lizlove <elizabethlovero@gmail.com>
2015-09-14 01:44:36 -0400

    owl makes api call

--- js/script.js
+++ js/script.js
@@ -1,54 +1,42 @@
-$(document).ready(function() {
-	// Build image carousel with responsive structure
-	$('.slider').slick({
-		lazyLoad: 'ondemand',
-		dots: true,
-		arrows: true,
-		infinite: true,
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 4,
-					slidesToScroll: 1,
-					infinite: true,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
-	});
+// $(document).ready(function() {
 
 	// Get images from mock API and append to image carousel
-	var swellAPI = 'http://builds.tryswell.com/mock/items.json';
-	$.getJSON(swellAPI, function(result){
-		result.items.forEach(function(shoe){
-			var id_number = "<h3>Tip " + shoe.id + "<h3/>";
-			var url = shoe.images[0]['url'];
-			var image = "<img src='" + url + "' class='item' height='200' width='200'/>";
-			var title = shoe.heading;
-			var copy = "<p>" + shoe.description + "</p>";
-			console.log(id_number, image, copy);
-			$('.slick-track').append(id_number, image, copy);
-		});
-	}); 
-});
+	// var swellAPI = 'http://builds.tryswell.com/mock/items.json';
+	// $.getJSON(swellAPI, function(result){
+	// 	result.items.forEach(function(shoe){
+	// 		var id_number = "<h3>Tip " + shoe.id + "<h3/>";
+	// 		var url = shoe.images[0]['url'];
+	// 		var image = "<img src='" + url + "' class='item' height='200' width='200'/>";
+	// 		var title = shoe.heading;
+	// 		var copy = "<p>" + shoe.description + "</p>";
+	// 		console.log(id_number, image, copy);
+	// 		$('.slick-track').append(id_number, image, copy);
+	// 	});
+	// }); 
+// });
 	 
-
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+  	items : 4, 
+    jsonPath : 'http://builds.tryswell.com/mock/items.json',
+    jsonSuccess : customDataSuccess
+  });
+ 
+  function customDataSuccess(data){
+    var content = "";
+    for(var i in data["items"]){
+       
+       var img = data["items"][i]["images"][0]['url'];
+       var alt = data["items"][i].heading;
+       var tip_id = data["items"][i].id;
+       var copy = data["items"][i].description;
+ 
+       content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
+    }
+    $("#owl-demo").html(content);
+  }
+ 
+});
 
 
